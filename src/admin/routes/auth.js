b1900110aaf6eec0cc0a0e53144a3f04afd8f846
nutan kumar <nutan2247@gmail.com>
2022-10-17 19:20:14 +0530

    new changes on cros

--- src/admin/routes/auth.js
+++ src/admin/routes/auth.js
@@ -10,6 +10,12 @@ const { check, validationResult } = require('express-validator');
 
 const Admin     = require('../models/admin'); 
 
+var cors = require('cors')
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+
 // @route    GET api/auth
 // @desc     Get user by token
 // @access   Private
@@ -29,7 +35,7 @@ router.get('/test', auth, async (req, res) => {
 router.post(
   '/login',
   check('email', 'Please include a valid email').isEmail(),
-  check('password', 'Password is required').exists(),
+  check('password', 'Password is required').exists(), cors(corsOptions),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
