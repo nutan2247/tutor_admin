bd1a078a26872f579ad670c723617133f7b8310a
Ijsharma123 <93309414+Ijsharma123@users.noreply.github.com>
2022-11-04 15:38:32 +0530

    change in subject

--- src/admin/models/subject.js
+++ src/admin/models/subject.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const bannerSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     sub_id: { type: Number, required: false },
-    sub_name : { type: String, required: true },
+    subject_name : { type: String, required: true },
     department_id: { type: String, required: true },
-    sub_status: { type: Number, required: true },
+    sub_status: { type: String, required: true },
     sub_date: { type: Date, required: false }
 });
 
