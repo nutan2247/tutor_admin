9eb9a1610df2c1e2384d261a5c7d7ceab42503ff
Ijsharma123 <93309414+Ijsharma123@users.noreply.github.com>
2022-12-12 17:16:47 +0530

    chapter topic api

--- src/admin/models/subject.js
+++ src/admin/models/subject.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const subjectSchema = mongoose.Schema({
     class_id:{type:String,required:true},
-    subject_name: { type: String, required: true },
+    name: { type: String, required: true },
     status: { type: String, required: true },
     image: { type: String, required: false },
     date: { type: Date, default:Date.now() },
