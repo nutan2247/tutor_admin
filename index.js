c4fa241c9b83029f8b0cf721bf6b05aea23fa334
Ijsharma123 <93309414+Ijsharma123@users.noreply.github.com>
2022-12-14 16:55:18 +0530

    changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-app.use("/images",express.static('uploads'))
+app.use("/uploads",express.static('uploads'))
 
 // app.use(cors({
 //     'allowedHeaders': ['sessionId', 'Content-Type'],
