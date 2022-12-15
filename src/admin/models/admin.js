636ab1daf8c5426a6147a4644c3cbf545628e741
Ijsharma123 <93309414+Ijsharma123@users.noreply.github.com>
2022-12-15 15:03:12 +0530

    changes in api

--- src/admin/models/admin.js
+++ src/admin/models/admin.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
     id: { type: Number, required: true, unique: true },
     email: { type: String, required: true, unique: true },
+    mobile_number: {type:String, required:true},
     first_name: { type: String, required: false },
     last_name: { type: String, required: false },
     password: { type: String, required: false },
